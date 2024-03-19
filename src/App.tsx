95f7607b57829931cc0cada183ce9bf47c7bcf76
Rajat Gupta <7rgdev@gmail.com>
2024-03-19 14:33:41 +0530

    Added Navbar & improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-
 import "./App.css";
 import MainPage from "./MainPage";
 
 function App() {
   return (
-    <div className="mt-4">
-      <header>
-        <MainPage />
+    <div className="flex flex-col min-h-screen">
+      <header className="border-b">
+        <div className="container px-4 py-2">
+          <nav className="flex items-center justify-between">
+            <div className="flex items-center space-x-4"></div>
+            <div className="flex-1 min-w-0 text-center">
+              <h1 className="text-lg font-bold tracking-tight">
+                Random Number Generator
+              </h1>
+            </div>
+            <div className="flex-1 min-w-0 text-right" />
+          </nav>
+        </div>
       </header>
+      <main className="flex-1 bg-gray-100 py-10 flex items-center justify-center">
+        <MainPage />
+      </main>
     </div>
   );
 }
