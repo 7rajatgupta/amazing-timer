056b2d5b988e23af3425ac18924f5cbd04dd79c1
Rajat Gupta <7rgdev@gmail.com>
2024-03-19 14:36:11 +0530

    Added Styling to Content and MainPage components

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -30,7 +30,11 @@ const Content: FC<{
   const randomNumber = useRandomNumber();
   // END - DO NOT EDIT
   const memoizedRandomNumber = useMemo(() => randomNumber, [open]);
-  return open ? <div>Your random number is: {memoizedRandomNumber}</div> : null;
+  return (
+    <h2 className="text-7xl font-bold tracking-tighter">
+      {open ? <>{memoizedRandomNumber}</> : <>🎲</>}
+    </h2>
+  );
 };
 
 const MainPage: FC = () => {
@@ -43,11 +47,27 @@ const MainPage: FC = () => {
   }
   return (
     <div>
-      <div>The page loaded {Math.floor(time / 1000)} seconds ago</div>
-      <button onClick={toggle}>
-        {open ? "Hide" : "Generate random number"}
-      </button>
-      <Content open={open} />
+      <div className="container flex flex-col items-center gap-4 px-4 text-center md:gap-8">
+        <div className="space-y-2">
+          <p className="text-gray-800 mb-40 text-2xl font-bold">
+            Click the button to generate a random number
+          </p>
+          <Content open={open} />
+        </div>
+        <button
+          className="mx-auto w-full max-w-xs h-10 items-center justify-center rounded-md border border-sky-800  bg-sky-600 text-md shadow-sm transition-colors hover:bg-sky-700 text-white font-bold"
+          onClick={toggle}
+        >
+          {open ? "Hide" : "Generate random number"}
+        </button>
+      </div>
+      <div className="py-4">
+        <div className="container flex items-center justify-center space-x-2 text-center">
+          <p className="text-md text-gray-700 dark:text-gray-400 mt-10">
+            Page loaded {Math.floor(time / 1000)} seconds ago
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
