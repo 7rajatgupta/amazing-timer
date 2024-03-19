16d61b0ea518b91a814ada89bc00725464bd0840
Rajat Gupta <7rgdev@gmail.com>
2024-03-19 14:52:17 +0530

    cleanup & added footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,31 @@ function App() {
     <div className="flex flex-col min-h-screen">
       <header className="border-b">
         <div className="container px-4 py-2">
-          <nav className="flex items-center justify-between">
-            <div className="flex items-center space-x-4"></div>
-            <div className="flex-1 min-w-0 text-center">
-              <h1 className="text-lg font-bold tracking-tight">
-                Random Number Generator
-              </h1>
-            </div>
-            <div className="flex-1 min-w-0 text-right" />
+          <nav className="flex">
+            <h1 className="text-lg font-bold tracking-tight">
+              Random Number Generator
+            </h1>
           </nav>
         </div>
       </header>
       <main className="flex-1 bg-gray-100 py-10 flex items-center justify-center">
         <MainPage />
       </main>
+      <div className="py-4">
+        <div className="container flex items-center justify-center space-x-2 text-center">
+          <p className="text-md text-gray-700 dark:text-gray-400">
+            crafted with ❤️ by{" "}
+            <a
+              className="text-black font-bold"
+              href="https://www.rajat-gupta.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Rajat Gupta
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
